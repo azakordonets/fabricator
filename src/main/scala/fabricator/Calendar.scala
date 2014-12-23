e8f169f970bca3f6180f976d697b0563b3e02d14
Andrew Zakordonets <biercoff@gmail.com>
2014-12-23 17:22:08 +0100

    refactored calendar class

--- src/main/scala/fabricator/Calendar.scala
+++ src/main/scala/fabricator/Calendar.scala
@@ -1,36 +1,132 @@
 package fabricator
 
-import com.github.nscala_time.time.Imports._
+import org.joda.time.{IllegalFieldValueException, DateTime}
+
+import scala.util.Random
 
 /**
  * Created by Andrew Zakordonets on 03/06/14.
  */
-class Calendar {
+class Calendar(private val utility: UtilityService,
+               private val random: Random,
+               private val alpha: Alphanumeric,
+               private val creditCard: CreditCard) {
 
-  def date() : String = {
+  def this() {
+    this(new UtilityService(), new Random(), new Alphanumeric(), new CreditCard)
+  }
+
+
+  def ampm(): String = {
+    val randomVal = alpha.integer(0, 10)
+    if (randomVal < 5) "am" else "pm"
+  }
+
+  def second(): String = {
+    alpha.integer(0, 59).toString
+  }
+
+  def minute(): String = {
+    alpha.integer(0, 59).toString
+  }
+
+  def hour(): String = {
+    hour(false)
+  }
+
+  def hour(twentyfour: Boolean): String = {
+    if (twentyfour) alpha.integer(0, 24).toString
+    else alpha.integer(1, 12).toString
+  }
+
+  def day(): String = {
+    alpha.integer(1, 31).toString
+  }
+
+  def month(): String = {
+    month(true)
+  }
+
+  def month(numberFormat: Boolean): String = {
+    if (numberFormat) alpha.integer(1, 12).toString
+    else utility.getValueFromArray("month")
+  }
+
+  def year(): String = {
+    alpha.integer(1970, 2015).toString
+  }
+
+  def date(): String = {
     date("dd-MM-yyyy")
   }
 
-  def date(format:String ): String = {
-    DateTime.now.toString(format)
+  def date(format: String): String = {
+    new DateTime(year().toInt, month().toInt, day().toInt, hour().toInt, minute().toInt, second().toInt).toString(format)
+  }
+
+  def dateObject(): DateTime = {
+    new DateTime(year().toInt, month().toInt, 1, hour().toInt, minute().toInt, second().toInt).plusDays(alpha.integer(1,31))
+  }
+
+  def date(year: Int, month: Int, day: Int, hour: Int, minute: Int): String = {
+    var date = ""
+    try{
+      while(date == "") {
+          date = new DateTime(year, month, day, hour, minute).toString("dd-MM-yyyy hh:mm")
+      }
+    }catch {
+      case e: IllegalFieldValueException => return this.date(year, month, day-1, hour, minute)
+    }
+    date
+  }
+
+  def date(year: Int, month: Int, day: Int, hour: Int, minute: Int, defFormat: String): String = {
+    new DateTime(day, month, year, hour, minute).toString(defFormat)
+  }
+
+  def dateWithPeriod(years: Int, months: Int, weeks: Int, days: Int, hours: Int, minutes: Int): String = {
+    dateWithPeriod(years, months, weeks, days, hours, minutes, "dd-MM-yyyy hh:mm")
+  }
+
+  def dateWithPeriod(years: Int, months: Int, weeks: Int, days: Int, hours: Int, minutes: Int, format: String): String = {
+    val date = DateTime.now()
+//    date.en
+    (years, months, weeks, days, hours, minutes) match {
+      case (0 , 0, 0, 0, 0, 0) if years > 0 => date.formatted(format)
+      case (years , 0, 0, 0, 0, 0) if years > 0 => date.plusYears(years).formatted(format)
+      case (years , 0, 0, 0, 0, 0) if years < 0=> date.minusYears(years).formatted(format)
+      case (0 , months, 0, 0, 0, 0) if months > 0 => date.plusMonths(months).formatted(format)
+      case (0 , months, 0, 0, 0, 0) if months < 0=> date.minusMonths(months).formatted(format)
+      case (0 , 0, weeks, 0, 0, 0) if weeks > 0 => date.plusWeeks(weeks).formatted(format)
+      case (0 , 0, weeks, 0, 0, 0) if weeks < 0=> date.minusWeeks(weeks).formatted(format)
+      case (0 , 0, 0, days, 0, 0) if days > 0 => date.plusDays(days).formatted(format)
+      case (0 , 0, 0, days, 0, 0) if days < 0=> date.minusDays(days).formatted(format)
+      case (0 , 0, 0, 0, hours, 0) if hours > 0 => date.plusHours(hours).formatted(format)
+      case (0 , 0, 0, 0, hours, 0) if hours < 0=> date.minusHours(hours).formatted(format)
+      case (0 , 0, 0, 0, 0, minutes) if minutes > 0 => date.plusMinutes(minutes).formatted(format)
+      case (0 , 0, 0, 0, 0, minutes) if minutes < 0=> date.minusMinutes(minutes).formatted(format)
+      case (years , months, 0, 0, 0, 0) if years > 0 && months > 0 => date.plusYears(years).plusMonths(months).formatted(format)
+      case (years , months, 0, 0, 0, 0) if years < 0 && months < 0 => date.minusYears(years).minusMonths(months).formatted(format)
+      case (years , months, weeks, 0, 0, 0) if years > 0 && months > 0 && weeks > 0 => date.plusYears(years).plusMonths(months).plusWeeks(weeks).formatted(format)
+      case (years , months, weeks, 0, 0, 0) if years < 0 && months < 0 && weeks < 0 => date.minusYears(years).minusMonths(months).minusWeeks(weeks).formatted(format)
+      case (years , months, weeks, days, 0, 0) if years > 0 && months > 0 && weeks > 0 && days > 0=> date.plusYears(years).plusMonths(months).plusWeeks(weeks).plusDays(days).formatted(format)
+      case (years , months, weeks, days, 0, 0) if years < 0 && months < 0 && weeks < 0 && days < 0 => date.minusYears(years).minusMonths(months).minusWeeks(weeks).minusDays(days).formatted(format)
+      case (years , months, weeks, days, hours, 0) if years > 0 && months > 0 && weeks > 0 && days > 0 && hours > 0 => date.plusYears(years).plusMonths(months).plusWeeks(weeks).plusDays(days).plusHours(hours).formatted(format)
+      case (years , months, weeks, days, hours, 0) if years < 0 && months < 0 && weeks < 0 && days < 0 && hours < 0 => date.minusYears(years).minusMonths(months).minusWeeks(weeks).minusDays(days).minusHours(hours).formatted(format)
+      case (years , months, weeks, days, hours, minutes) if years > 0 && months > 0 && weeks > 0 && days > 0 && hours > 0 && minutes > 0 => date.plusYears(years).plusMonths(months).plusWeeks(weeks).plusDays(days).plusHours(hours).plusMinutes(minutes).formatted(format)
+      case (years , months, weeks, days, hours, minutes) if years < 0 && months < 0 && weeks < 0 && days < 0 && hours < 0 && minutes < 0=> date.minusYears(years).minusMonths(months).minusWeeks(weeks).minusDays(days).minusHours(hours).minusMinutes(minutes).formatted(format)
+      case _ => throw new Exception ("Invalid format of values. Values can be either 0, or > 0, or < 0. You're not allowed to enter +3 days - 2 hours. ")
+    }
+
   }
 
 
-
-//  def date(dayOfTheYear:Int = 0, monthOfTheYear:Int = 0, yearVal:Int = 0, hour:Int = 0, minute:Int = 0, defFormat: String = "dd-mm-yyyy"): String = {
-//    new DateTime(yearVal, monthOfTheYear, dayOfTheYear, hour, minute).toString(defFormat)
-//  }
-
-  def checkDay() = {
-
-  }
-
-  def leapYear(year: Int):Boolean = {
+  def isLeapYear(year: Int): Boolean = {
     0 == year % 4 && 0 != year % 100 || 0 == year % 400
   }
 
-  def checkYear(year: Int):Boolean = year match{
-    case year if ((1900 to 3000).contains(year))  => true
+  def checkYear(year: Int): Boolean = year match {
+    case year if ((1900 to 3000).contains(year)) => true
     case _ => false
   }
 
--- src/test/scala/fabricator/CalendarTestSuite.scala
+++ src/test/scala/fabricator/CalendarTestSuite.scala
@@ -5,7 +5,7 @@ import com.typesafe.scalalogging.slf4j.LazyLogging
 import org.scalatest.testng.TestNGSuite
 import org.testng.annotations.{DataProvider, Test}
 
-class CalendarTestSuite extends TestNGSuite with LazyLogging{
+class CalendarTestSuite extends TestNGSuite with LazyLogging {
 
   val fabr = new Fabricator
   val util = new UtilityService()
@@ -13,87 +13,89 @@ class CalendarTestSuite extends TestNGSuite with LazyLogging{
 
   @Test
   def testDefaultDateGetter() = {
-    logger.info("Checking default date value " + calendar.date())
-    assertResult(calendar.date())(DateTime.now.toString("dd-MM-yyyy"))
+    val date = calendar.date()
+    logger.info("Checking default date value " + date)
+    assert(date.matches("\\d{2}-\\d{2}-\\d{4}"))
   }
 
   @DataProvider(name = "dateFormats")
   def dateFormats() = {
-    Array(Array("dd:mm:yyyy"),
-      Array("mm:dd:yyyy"),
-      Array("dd:mm:yyyy"),
-      Array("dd:MM:yyyy"),
-      Array("dd:MM:YYYY"),
-      Array("dd/MM/YYYY"),
-      Array("dd/MM/YY"),
-      Array("dd-MM-yyyy"),
-      Array("dd.MM.yyyy"),
-      Array("dd.M.yyyy"),
-      Array("dd-MM-yyyy HH"),
-      Array("dd-MM-yyyy HH:mm"),
-      Array("dd-MM-yyyy HH:mm:ss"),
-      Array("dd-MM-yyyy H:m:s"),
-      Array("dd-MM-yyyy H:m:s a")
+    Array(Array("dd:mm:yyyy", "\\d{2}:\\d{2}:\\d{4}"),
+      Array("mm:dd:yyyy", "\\d{2}:\\d{2}:\\d{4}"),
+      Array("dd:mm:yyyy", "\\d{2}:\\d{2}:\\d{4}"),
+      Array("dd:MM:yyyy", "\\d{2}:\\d{2}:\\d{4}"),
+      Array("dd:MM:YYYY", "\\d{2}:\\d{2}:\\d{4}"),
+      Array("dd/MM/YYYY", "\\d{2}/\\d{2}/\\d{4}"),
+      Array("dd/MM/YY", "\\d{2}/\\d{2}/\\d{2}"),
+      Array("dd-MM-yyyy", "\\d{2}-\\d{2}-\\d{4}"),
+      Array("dd.MM.yyyy", "\\d{2}\\.\\d{2}\\.\\d{4}"),
+      Array("dd.M.yyyy", "\\d{2}\\.\\d{1,2}\\.\\d{4}"),
+      Array("dd-MM-yyyy HH", "\\d{2}-\\d{2}-\\d{4} \\d{2}"),
+      Array("dd-MM-yyyy HH:mm", "\\d{2}-\\d{2}-\\d{4} \\d{2}:\\d{2}"),
+      Array("dd-MM-yyyy HH:mm:ss", "\\d{2}-\\d{2}-\\d{4} \\d{2}:\\d{2}:\\d{2}"),
+      Array("dd-MM-yyyy H:m:s", "\\d{1,2}-\\d{1,2}-\\d{4} \\d{1,2}:\\d{1,2}:\\d{1,2}"),
+      Array("dd-MM-yyyy H:m:s a", "\\d{1,2}-\\d{1,2}-\\d{4} \\d{1,2}:\\d{1,2}:\\d{1,2} \\w{2}")
     )
   }
 
   @Test(dataProvider = "dateFormats")
-  def testDateGetterWithDifferentFormats(format: String) = {
-    logger.info("Checking date value with " + format + " format :" + calendar.date(format))
-    assertResult(calendar.date(format))(DateTime.now.toString(format))
+  def testDateGetterWithDifferentFormats(format: String, regex: String) = {
+    val date = calendar.date(format)
+    logger.info("Checking date value with " + format + " format :" + date)
+    assert(date.matches(regex))
   }
 
-  @DataProvider(name = "datesVariations")
-  def datesVariations() = {
-    Array(Array("1", "6", "1800", "00", "00", "dd-mm-yyy hh:mm a", "01-00-1800 12:00 AM"),
-      Array("12", "1", "2012", "02", "10", "dd-mm-yyy hh:mm", "12-10-2012 02:10"),
-      Array("1", "2", "2014", "14", "10", "dd-mm-yyy hh:mm a", "01-10-2014 02:10 PM"),
-      Array("10", "3", "2014", "14", "10", "dd/mm/yyy hh:mm", "10/10/2014 02:10"),
-      Array("20", "4", "2014", "14", "10", "dd.mm.yyy hh-mm", "20.10.2014 02-10"),
-      Array("20", "4", "2014", "14", "10", "dd.mm.yyy hh-mm-ss", "20.10.2014 02-10-00"),
-      Array("13", "5", "2014", "14", "10", "ddmmyyy hh:mm", "13102014 02:10"),
-      Array("16", "6", "2014", "14", "10", "dd-mm-yyy hh", "16-10-2014 02"),
-      Array("18", "7", "2014", "14", "10", "dd-mm-yyy", "18-10-2014"),
-      Array("20", "8", "2014", "14", "10", "dd-mm-yy hh:mm", "20-10-14 02:10"),
-      Array("30", "9", "2014", "14", "10", "dd-MM-yyy hh:mm", "30-09-2014 02:10"),
-      Array("18", "10", "2014", "14", "10", "dd-mm-yyyy hh:mm", "18-10-2014 02:10"),
-      Array("18", "11", "2014", "14", "10", "dd-mm-y hh:mm", "18-10-2014 02:10"),
-      Array("02", "12", "2014", "14", "10", "d-mm-yyy hh:mm", "2-10-2014 02:10"),
-      Array("5", "6", "2014", "14", "10", "d-m-yyyy hh:mm", "5-10-2014 02:10"),
-      Array("14", "6", "2014", "14", "10", "d-m-y h:m", "14-10-2014 2:10"),
-      Array("14", "6", "2014", "14", "10", "dd-mmm-yyyy h:m", "14-010-2014 2:10"),
-      Array("12", "6", "2014", "14", "10", "dd MMM yyyy h:m", "12 Jun 2014 2:10"),
-      Array("12", "6", "2014", "14", "10", "dd MMMM yyyy h:m", "12 June 2014 2:10")
-    )
+
+  @Test
+  def testAmPm() = {
+    val ampm = calendar.ampm()
+    logger.info("Testing random amPm value : " + ampm)
+    assert(ampm.equals("am") || ampm.equals("pm"))
   }
 
-  //  @Test(dataProvider = "datesVariations")
-  //  def testDatesVariations(day:String, month:String, year:String, hour:String, minute:String, format:String, expectedResult: String) {
-  //    val date = calendar.date(day.toInt, month.toInt, year.toInt, hour.toInt, minute.toInt, format)
-  //    logger.info("Checking date value with "+format+" format :"+date)
-  //    assertResult(date)(expectedResult)
-  //  }
-
-  @DataProvider(name = "datesNegativeVariations")
-  def datesNegativeVariations() = {
-    Array(
-      Array("31", "6", "1800", "00", "00", "dd-mm-yyy hh:mm a", "01-00-1800 12:00 AM"),
-      Array("0", "1", "2012", "02", "10", "dd-mm-yyy hh:mm", "12-10-2012 02:10"),
-      Array("-1", "1", "2012", "02", "10", "dd-mm-yyy hh:mm", "12-10-2012 02:10"),
-      Array("1", "-1", "2012", "02", "10", "dd-mm-yyy hh:mm", "12-10-2012 02:10"),
-      Array("1", "1", "2012", "-02", "10", "dd-mm-yyy hh:mm", "12-10-2012 02:10"),
-      Array("1", "1", "2012", "02", "-10", "dd-mm-yyy hh:mm", "12-10-2012 02:10"),
-      Array("1", "1", "2012", "02", "10", "ffffffff", "12-10-2012 02:10"),
-      Array("", "", "", "", "", "", "12-10-2012 02:10")
-    )
+  @Test
+  def testSecond() = {
+    val second = calendar.second()
+    logger.info("Testing random second value: " + second)
+    assert(second.toInt > 0 && second.toInt <= 60)
   }
 
-  //  @Test(dataProvider = "datesNegativeVariations")
-  //  def datesNegativeVariationsTest(day:String, month:String, year:String, hour:String, minute:String, format:String, expectedResult: String) {
-  //    intercept[Exception]{
-  //        val date = calendar.date(day.toInt, month.toInt, year.toInt, hour.toInt, minute.toInt, format)
-  //        logger.info("Checking date value with "+format+" format :"+date)
-  //    }
-  //  }
+  @Test
+  def testMinute() = {
+    val minute = calendar.minute()
+    logger.info("Testing random minute value: " + minute)
+    assert(minute.toInt > 0 && minute.toInt < 60)
+  }
+  
+  @Test
+  def testHour() = {
+    val hour24 = calendar.hour(true)
+    val hour12 = calendar.hour()
+    assert(hour24.toInt >= 0 && hour24.toInt < 24)
+    assert(hour12.toInt >= 0 && hour12.toInt < 12)
+  }
+  
+  @Test
+  def testDay() = {
+    val day = calendar.day()
+    logger.info("Testing random day value: " + day)
+    assert(day.toInt > 0 && day.toInt < 31)
+  }
+
+  @DataProvider
+  def dateDP() = {
+    Array(Array(2014, 02, 30, 00, 00, "28-02-2014 12:00"),
+      Array(1000, 02, 30, 00, 00, "28-02-1000 12:00"),
+      Array(1980, 01, 50, 12, 30, "31-01-1980 12:30"),
+      Array(2250, 02, 30, 00, 00, "28-02-2250 12:00"))
+  }
+
+  @Test(dataProvider = "dateDP")
+  def testDate(year: Int, month: Int, day: Int, hour: Int, minute: Int, expectedResult: String) = {
+    val date = calendar.date(year, month, day, hour, minute)
+    logger.info("Testing random date: " + date)
+    assert(date.equals(expectedResult))
+  }
+
 
 }
