c6644131c0613c80efdebff1315e4544491e86da
Andrew Zakordonets <biercoff@gmail.com>
2015-06-18 23:48:06 +0200

    add fix for failing JavaInternetTestSuite

--- src/test/java/fabricator/j/tests/JavaInternetTestSuite.java
+++ src/test/java/fabricator/j/tests/JavaInternetTestSuite.java
@@ -2,9 +2,12 @@ package fabricator.j.tests;
 
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class JavaInternetTestSuite extends JavaBaseTest {
 
@@ -15,6 +18,11 @@ public class JavaInternetTestSuite extends JavaBaseTest {
         stringStringHashMap.put("hello2", "test2");
         stringStringHashMap.put("hello3", "test 3");
         final String url = internet.urlBuilder().host("test.ru").paramsinJava(stringStringHashMap).toString();
-        assertEquals("http://test.ru/getEntity?hello=test&hello2=test2&hello3=test+3", url);
+        assertTrue(url.matches("^(.*:)//([A-Za-z0-9\\-\\.]+)(:[0-9]+)?(.*)$"));
+        String params = url.split("/")[3];
+        List<String> stringList = Arrays.asList(params.split("&"));
+        assertTrue(stringList.contains("getEntity?hello=test"));
+        assertTrue(stringList.contains("hello2=test2"));
+        assertTrue(stringList.contains("hello3=test+3"));
     }
 }
