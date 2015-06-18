82ed64d8a72d093a48b2e025aa17d22c0960d6f9
Andrew Zakordonets <biercoff@gmail.com>
2015-06-18 17:17:47 +0200

    fix java calendar test

--- src/test/java/fabricator/j/tests/CalendarJavaTest.java
+++ src/test/java/fabricator/j/tests/CalendarJavaTest.java
@@ -1,5 +1,6 @@
 package fabricator.j.tests;
 
+import fabricator.enums.DateFormat;
 import fabricator.enums.DateRangeType;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -16,7 +17,7 @@ public class CalendarJavaTest extends JavaBaseTest {
 		final List<String> yearRange = calendar.datesRange()
 				.startYear(2001)
 				.startMonth(1)
-				.startMonth(1)
+				.startDay(1)
 				.stepEvery(1, DateRangeType.YEARS)
 				.endYear(2010)
 				.endMonth(1)
@@ -32,10 +33,10 @@ public class CalendarJavaTest extends JavaBaseTest {
 				{ 2001, 1, 1, 2010, 1, 1,DateRangeType.YEARS, 2, 5 },
 				{ 2001, 1, 1, 2010, 1, 1, DateRangeType.MONTHS, 1, 108},
 				{ 2001, 1, 1, 2010, 1, 1,DateRangeType.MONTHS, 2, 54},
-				{ 2001, 1, 1, 2010, 1, 1,DateRangeType.WEEKS, 2, 234},
-				{ 2001, 1, 1, 2001, 10, 1,DateRangeType.DAYS, 10, 26},
-				{ 2001, 1, 1, 2001, 2, 1,DateRangeType.HOURS, 10, 36},
-				{ 2001, 1, 1, 2001, 2, 1,DateRangeType.MINUTES, 10, 2160},
+				{ 2001, 1, 1, 2010, 1, 1,DateRangeType.WEEKS, 2, 235},
+				{ 2001, 1, 1, 2001, 10, 1,DateRangeType.DAYS, 10, 28},
+				{ 2001, 1, 1, 2001, 2, 1,DateRangeType.HOURS, 10, 75},
+				{ 2001, 1, 1, 2001, 1, 2,DateRangeType.MINUTES, 1, 1440}
 		};
 	}
 
@@ -43,9 +44,10 @@ public class CalendarJavaTest extends JavaBaseTest {
 	public void testDatesRange(int startYear, int startMonth, int startDay, int endYear, int endMonth, int endDay, DateRangeType type, int step, int expectedSize) {
 		final List<String> datesRange = calendar.datesRange()
 												.startYear(startYear)
+												.startDay(startDay)
 												.startMonth(startMonth)
-												.startMonth(startDay)
 												.stepEvery(step, type)
+												.format(DateFormat.dd_MM_yyyy_HH_mm)
 												.endYear(endYear)
 												.endMonth(endMonth)
 												.endDay(endDay)
