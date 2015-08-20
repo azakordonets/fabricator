c65995da34621d349f46ac890c1931a2ca065345
Andrew Zakordonets <biercoff@gmail.com>
2015-08-20 11:15:53 +0200

    introduce CsvFileBuilder and refactor FileGenerator to use builder

--- src/main/scala/fabricator/FileGenerator.scala
+++ src/main/scala/fabricator/FileGenerator.scala
@@ -1,12 +1,11 @@
 package fabricator
 
-import java.awt._
 import java.awt.font.{FontRenderContext, TextLayout}
 import java.awt.image.BufferedImage
-import java.io.File
+import java.awt.{Color, Font, Graphics2D, Rectangle}
 
-import com.github.tototoshi.csv._
-import fabricator.enums.{CsvValueCode, FileType, MimeType}
+import fabricator.entities.CsvFileBuilder
+import fabricator.enums.{FileType, MimeType}
 
 import scala.util.Random
 
@@ -61,68 +60,8 @@ class FileGenerator(private val alpha: Alphanumeric,
 
   }
 
-
-  def csv(): Unit = {
-    val values = Array(CsvValueCode.FIRST_NAME,
-                        CsvValueCode.LAST_NAME,
-                        CsvValueCode.BIRTHDAY,
-                        CsvValueCode.EMAIL,
-                        CsvValueCode.PHONE,
-                        CsvValueCode.ADDRESS,
-                        CsvValueCode.BSN,
-                        CsvValueCode.WEIGHT,
-                        CsvValueCode.HEIGHT)
-    // create temporary generatedFiles dir
-    val dir: File = new File("generatedFiles")
-    dir.mkdir
-    // save it in this folder by default result.
-    csvFromCodes(values, 100, "generatedFiles/result.csv", ',')
-  }
-
-
-  def csv(seq: Seq[Any], rows: Int, path: String): Unit = csv(seq, rows, path, ',')
-
-  def csv(seq: Seq[Any], rows: Int, path: String, customDelimiter: Char): Unit = {
-    val expectedFile = new File(path)
-    if (!expectedFile.exists) expectedFile.createNewFile
-    implicit object MyFormat extends DefaultCSVFormat {
-      override val delimiter = customDelimiter
-    }
-    val writer = CSVWriter.open(expectedFile)
-    for (i <- 0 to rows - 1) {
-      writer.writeRow(seq)
-    }
-    writer.close()
-  }
-
-  def csvFromCodes(codes: Array[CsvValueCode], rows: Int, path: String): Unit = csvFromCodes(codes, rows, path, ',')
-
-  def csvFromCodes(codes: Array[CsvValueCode], rows: Int, path: String, customDelimiter: Char): Unit = {
-    writeCsvFileData(generateTitles(codes, customDelimiter), codes, rows, path, customDelimiter)
-  }
-
-  private def writeCsvFileData(titles: Array[String], values: Seq[CsvValueCode], rows: Int, path: String, delimeter: Char) = {
-    val expectedFile = new File(path)
-    if (!expectedFile.exists) expectedFile.createNewFile
-    implicit object MyFormat extends DefaultCSVFormat {
-      override val delimiter = delimeter
-    }
-    val writer = CSVWriter.open(expectedFile)
-    writer.writeRow(titles)
-    for (i <- 0 to rows - 1) {
-      val generatedMap = values.map(x => generateValue(x))
-      writer.writeRow(generatedMap)
-    }
-    writer.close()
-  }
-
-  def generateTitles(codes: Array[CsvValueCode], delimeter: Char): Array[String] = {
-    val titles = new Array[String](codes.length)
-    for (i <- codes.indices) {
-      titles(i) = codes(i).getTitle
-    }
-    titles
-  }
+  def csvBuilder : CsvFileBuilder = new CsvFileBuilder(alpha, calendar, contact, finance, internet,
+                                                          location, mobile, user_agent, words)
 
   def fileName: String = fileName(FileType.getRandom)
 
@@ -158,57 +97,6 @@ class FileGenerator(private val alpha: Alphanumeric,
       case MimeType.MULTIPART => utility.getValueFromArray("multipart_mime_types")
       case MimeType.TEXT => utility.getValueFromArray("text_mime_types")
       case MimeType.VIDEO => utility.getValueFromArray("video_mime_types")
-    }
-  }
-
-
-  private def generateValue(code: CsvValueCode): String = {
-    code match {
-      case CsvValueCode.INTEGER => alpha.randomInt.toString
-      case CsvValueCode.DOUBLE => alpha.randomDouble.toString
-      case CsvValueCode.HASH => alpha.randomHash
-      case CsvValueCode.GUID => alpha.randomGuid
-      case CsvValueCode.TIME => calendar.time24h
-      case CsvValueCode.DATE => calendar.randomDate.asString
-      case CsvValueCode.NAME => contact.fullName(setPrefix = false, setSuffix = false)
-      case CsvValueCode.FIRST_NAME => contact.firstName
-      case CsvValueCode.LAST_NAME => contact.lastName
-      case CsvValueCode.BIRTHDAY => calendar.randomDate.inYear(alpha.randomInt(1900, 2000)).asString
-      case CsvValueCode.EMAIL => contact.eMail
-      case CsvValueCode.PHONE => contact.phoneNumber
-      case CsvValueCode.ADDRESS => contact.address
-      case CsvValueCode.POSTCODE => contact.postcode
-      case CsvValueCode.BSN => contact.bsn
-      case CsvValueCode.HEIGHT => contact.height(cm = false)
-      case CsvValueCode.WEIGHT => contact.weight(metric = true)
-      case CsvValueCode.OCCUPATION => contact.occupation
-      case CsvValueCode.VISA => finance.visacreditCard
-      case CsvValueCode.MASTER => finance.mastercreditCard
-      case CsvValueCode.IBAN => finance.iban
-      case CsvValueCode.BIC => finance.bic
-      case CsvValueCode.SSN => finance.ssn
-      case CsvValueCode.URL => internet.urlBuilder.toString()
-      case CsvValueCode.IP => internet.ip
-      case CsvValueCode.MACADDRESS => internet.macAddress
-      case CsvValueCode.UUID => internet.UUID
-      case CsvValueCode.COLOR => internet.color
-      case CsvValueCode.TWITTER => internet.twitter
-      case CsvValueCode.HASHTAG => internet.hashtag
-      case CsvValueCode.FACEBOOK => internet.facebookId
-      case CsvValueCode.GOOGLE_ANALYTICS => internet.googleAnalyticsTrackCode
-      case CsvValueCode.ALTITUDE => location.altitude
-      case CsvValueCode.DEPTH => location.depth
-      case CsvValueCode.LATITUDE => location.latitude
-      case CsvValueCode.LONGITUDE => location.longitude
-      case CsvValueCode.COORDINATES => location.coordinates
-      case CsvValueCode.GEOHASH => location.geohash
-      case CsvValueCode.APPLE_TOKEN => mobile.applePushToken
-      case CsvValueCode.ANDROID => mobile.androidGsmId
-      case CsvValueCode.WINDOWS7TOKEN => mobile.wp7_anid
-      case CsvValueCode.WINDOWS8TOKEN => mobile.wp8_anid2
-      case CsvValueCode.USER_AGENT => user_agent.chrome
-      case CsvValueCode.WORD => words.word
-      case CsvValueCode.SENTENCE => words.sentence(10)
     }
   }
 
---
+++ src/main/scala/fabricator/entities/CsvFileBuilder.scala
@@ -0,0 +1,156 @@
+package fabricator.entities
+
+import java.io.File
+
+import com.github.tototoshi.csv.{CSVWriter, DefaultCSVFormat}
+import fabricator._
+import fabricator.enums.CsvValueCode
+import fabricator.enums.CsvValueCode._
+
+class CsvFileBuilder(alpha: Alphanumeric,
+                      calendar: Calendar,
+                      contact: Contact,
+                      finance: Finance,
+                      internet: Internet,
+                      location: Location,
+                      mobile: Mobile,
+                      userAgent: UserAgent,
+                      words: Words) {
+
+  private var filePath: String = "generatedFiles/result.csv"
+
+  private var titles: Array[String] = null
+
+  private var codes: Array[CsvValueCode] = Array(FIRST_NAME,
+                                               LAST_NAME,
+                                               BIRTHDAY,
+                                               EMAIL,
+                                               PHONE,
+                                               ADDRESS,
+                                               BSN,
+                                               WEIGHT,
+                                               HEIGHT)
+
+  private var customValuesArray: Array[Any] = null
+
+  private var numberOfRows: Int = 10
+
+  private var customDelimiter: Char = ','
+
+  private var appendCsvFile: Boolean = false
+
+  def saveTo(filePath: String): this.type = {
+    this.filePath = filePath
+    this
+  }
+
+  def withCodes(codes: Array[CsvValueCode]): this.type = {
+    this.codes = codes
+    this
+  }
+
+  def withCustomCodes(values: Array[Any]): this.type = {
+    this.customValuesArray = values
+    this
+  }
+
+  def withTitles(titles: Array[String]): this.type = {
+    this.titles = titles
+    this
+  }
+
+  def withDelimiter(delimiter: Char): this.type = {
+    this.customDelimiter = delimiter
+    this
+  }
+
+  def withNumberOfRows(rows: Int): this.type = {
+    this.numberOfRows = rows
+    this
+  }
+
+  def addToExistingFile(): this.type = {
+    this.appendCsvFile = true
+    this
+  }
+
+  def build() = {
+    val expectedFile = new File(this.filePath)
+    if (!expectedFile.exists) expectedFile.createNewFile
+    implicit object MyFormat extends DefaultCSVFormat {
+      override val delimiter = customDelimiter
+    }
+    val writer = CSVWriter.open(expectedFile)
+    if (null == titles) generateTitles()
+    writer.writeRow(titles)
+    for (i <- 0 to numberOfRows - 1) {
+      if (null == customValuesArray) {
+        val generatedMap = codes.map(code => generateValue(code))
+        writer.writeRow(generatedMap)
+      } else {
+        val generatedMap = customValuesArray.map(value => generateValue(value))
+        writer.writeRow(generatedMap)
+      }
+    }
+    writer.close()
+  }
+
+
+  private def generateValue(code: Any): String = {
+    code match {
+      case CsvValueCode.INTEGER => alpha.randomInt.toString
+      case CsvValueCode.DOUBLE => alpha.randomDouble.toString
+      case CsvValueCode.HASH => alpha.randomHash
+      case CsvValueCode.GUID => alpha.randomGuid
+      case CsvValueCode.TIME => calendar.time24h
+      case CsvValueCode.DATE => calendar.randomDate.asString
+      case CsvValueCode.NAME => contact.fullName(setPrefix = false, setSuffix = false)
+      case CsvValueCode.FIRST_NAME => contact.firstName
+      case CsvValueCode.LAST_NAME => contact.lastName
+      case CsvValueCode.BIRTHDAY => calendar.randomDate.inYear(alpha.randomInt(1900, 2000)).asString
+      case CsvValueCode.EMAIL => contact.eMail
+      case CsvValueCode.PHONE => contact.phoneNumber
+      case CsvValueCode.ADDRESS => contact.address
+      case CsvValueCode.POSTCODE => contact.postcode
+      case CsvValueCode.BSN => contact.bsn
+      case CsvValueCode.HEIGHT => contact.height(cm = false)
+      case CsvValueCode.WEIGHT => contact.weight(metric = true)
+      case CsvValueCode.OCCUPATION => contact.occupation
+      case CsvValueCode.VISA => finance.visacreditCard
+      case CsvValueCode.MASTER => finance.mastercreditCard
+      case CsvValueCode.IBAN => finance.iban
+      case CsvValueCode.BIC => finance.bic
+      case CsvValueCode.SSN => finance.ssn
+      case CsvValueCode.URL => internet.urlBuilder.toString()
+      case CsvValueCode.IP => internet.ip
+      case CsvValueCode.MACADDRESS => internet.macAddress
+      case CsvValueCode.UUID => internet.UUID
+      case CsvValueCode.COLOR => internet.color
+      case CsvValueCode.TWITTER => internet.twitter
+      case CsvValueCode.HASHTAG => internet.hashtag
+      case CsvValueCode.FACEBOOK => internet.facebookId
+      case CsvValueCode.GOOGLE_ANALYTICS => internet.googleAnalyticsTrackCode
+      case CsvValueCode.ALTITUDE => location.altitude
+      case CsvValueCode.DEPTH => location.depth
+      case CsvValueCode.LATITUDE => location.latitude
+      case CsvValueCode.LONGITUDE => location.longitude
+      case CsvValueCode.COORDINATES => location.coordinates
+      case CsvValueCode.GEOHASH => location.geohash
+      case CsvValueCode.APPLE_TOKEN => mobile.applePushToken
+      case CsvValueCode.ANDROID => mobile.androidGsmId
+      case CsvValueCode.WINDOWS7TOKEN => mobile.wp7_anid
+      case CsvValueCode.WINDOWS8TOKEN => mobile.wp8_anid2
+      case CsvValueCode.USER_AGENT_CHROME => userAgent.chrome
+      case CsvValueCode.USER_AGENT_FIREFOX => userAgent.firefox
+      case CsvValueCode.USER_AGENT_IE => userAgent.internet_explorer
+      case CsvValueCode.USER_AGENT_OPERA => userAgent.opera
+      case CsvValueCode.WORD => words.word
+      case CsvValueCode.SENTENCE => words.sentence(10)
+      case _ => code.toString
+    }
+  }
+
+  def generateTitles(): Unit = {
+    titles = for (i <- codes) yield i.getTitle
+  }
+}
