4a2ac04ab36d809918890c33618db5bbc6cc5871
Andrew Zakordonets <biercoff@gmail.com>
2017-07-21 14:42:37 +0200

    change logger and specify types of each variable in BaseTestSuite

--- src/test/scala/fabricator/BaseTestSuite.scala
+++ src/test/scala/fabricator/BaseTestSuite.scala
@@ -1,22 +1,22 @@
 package fabricator
 
-import com.typesafe.scalalogging.slf4j.LazyLogging
+import com.typesafe.scalalogging.LazyLogging
 import org.scalatest.testng.TestNGSuite
 import org.testng.annotations.Test
 
 class BaseTestSuite extends TestNGSuite with LazyLogging {
 
-  lazy val util = UtilityService()
-  lazy val alpha = Fabricator.alphaNumeric()
-  lazy val location = Fabricator.location()
-  lazy val calendar = Fabricator.calendar()
-  lazy val contact = Fabricator.contact()
-  lazy val finance = Fabricator.finance()
-  lazy val internet = Fabricator.internet()
-  lazy val mobile = Fabricator.mobile()
-  lazy val file = Fabricator.file()
+  lazy val util: UtilityService = UtilityService()
+  lazy val alpha: Alphanumeric = Fabricator.alphaNumeric()
+  lazy val location: Location = Fabricator.location()
+  lazy val calendar: Calendar = Fabricator.calendar()
+  lazy val contact: Contact = Fabricator.contact()
+  lazy val finance: Finance = Fabricator.finance()
+  lazy val internet: Internet = Fabricator.internet()
+  lazy val mobile: Mobile = Fabricator.mobile()
+  lazy val file: FileGenerator = Fabricator.file()
   lazy val debugEnabled: Boolean = util.getProperty("loggerDebug").equals("true")
-  var words = Fabricator.words()
+  var words: Words = Fabricator.words()
 
   @Test(expectedExceptions = Array(classOf[Exception]))
   def testUtilityLessException() = {
