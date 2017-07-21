cbe29f0e4dbece26b3920031efc06a6bc683b07b
Andrew Zakordonets <biercoff@gmail.com>
2017-07-21 14:50:48 +0200

    use pattern matcher in JavaInternetTestSuite for better performance

--- src/test/java/fabricator/j/tests/JavaInternetTestSuite.java
+++ src/test/java/fabricator/j/tests/JavaInternetTestSuite.java
@@ -5,10 +5,14 @@ import org.testng.annotations.Test;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Pattern;
 
 import static org.testng.Assert.assertTrue;
 
 public class JavaInternetTestSuite extends JavaBaseTest {
+
+    private static final Pattern URL_PATTERN = Pattern.compile("^(.*:)//([A-Za-z0-9\\-\\.]+)(:[0-9]+)?(.*)$");
+    private static final Pattern PARAMS_PATTERN = Pattern.compile("getEntity\\?");
 
     @Test
     public void testUrl() {
@@ -17,9 +21,9 @@ public class JavaInternetTestSuite extends JavaBaseTest {
         stringStringHashMap.put("hello2", "test2");
         stringStringHashMap.put("hello3", "test 3");
         final String url = internet.urlBuilder().host("test.ru").paramsInJava(stringStringHashMap).toString();
-        assertTrue(url.matches("^(.*:)//([A-Za-z0-9\\-\\.]+)(:[0-9]+)?(.*)$"));
+        assertTrue(URL_PATTERN.matcher(url).matches());
         String params = url.split("/")[3];
-        List<String> paramsList = Arrays.asList(params.replaceAll("getEntity\\?","").split("&"));
+        List<String> paramsList = Arrays.asList(PARAMS_PATTERN.matcher(params).replaceAll("").split("&"));
         assertTrue(paramsList.contains("hello=test"));
         assertTrue(paramsList.contains("hello2=test2"));
         assertTrue(paramsList.contains("hello3=test+3"));
